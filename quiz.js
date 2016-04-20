c87ed32b34d92c32d07471094e95c7784d849904
Alan Eatherly <eatherlyae@gmail.com>
2016-04-20 14:13:36 -0500

    Code Tree in JS with set inputs

---
+++ quiz.js
@@ -0,0 +1,25 @@
+console.log("Begin Program");
+
+var prince = "*"; // symbol used to create the tree, specified by the user
+
+var willisT = 7; // height of the tree, specified by the user
+
+var space = " "; // var for space to left of symbol chose
+
+var planet // planet will be equal to the number of spaces needed
+
+var numP // numP will be equal to the number of symbols needed
+
+for (var i = 1; i <= willisT; i++) {
+
+  // assigning a value to planet, height of tree - i
+  planet = willisT - i;
+
+  // assigning a value to numP, proportional to i, always odd for symmetry
+  numP = (2 * i) - 1;
+
+  console.log("",space.repeat(planet) + prince.repeat(numP) + "\n");
+}
+
+
+console.log("End Program");
