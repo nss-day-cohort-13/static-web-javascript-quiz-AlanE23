7fc580b9f62ac3e8575cf26707fcccfe003284b2
Alan Eatherly <eatherlyae@gmail.com>
2016-04-21 11:03:40 -0500

    Link DOM inputs to JS

--- quiz.js
+++ quiz.js
@@ -1,14 +1,27 @@
 console.log("Begin Program");
 
-var prince = "*"; // symbol used to create the tree, specified by the user
+var prince // symbol used to create the tree, specified by the user
 
-var willisT = 7; // height of the tree, specified by the user
+var willisT // height of the tree, specified by the user
 
 var space = " "; // var for space to left of symbol chose
 
 var planet // planet will be equal to the number of spaces needed
 
 var numP // numP will be equal to the number of symbols needed
+
+var buttonPress = document.getElementById("buttonID");
+
+function submit () {
+  var prince = document.getElementById("bSymbol").value;
+
+  var willisT = document.getElementById("bHeight").value;
+
+  console.log("symbol", prince);
+  console.log("height", willisT);
+}
+
+
 
 for (var i = 1; i <= willisT; i++) {
 
@@ -23,3 +36,8 @@ for (var i = 1; i <= willisT; i++) {
 
 
 console.log("End Program");
+
+
+// empty movement() variable that becomes a function
+
+// "pass" arguement inside a function
